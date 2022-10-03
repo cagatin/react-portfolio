3d0687777a60ca5a57992bed485f81600c11c82e
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-03 11:37:57 -0700

    Changed navbar toggler color on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../styles/NavBar.css';
 export default function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light fixed-top d-flex">
-            <button className="navbar-toggler navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarNav"
+            <button className="navbar-toggler navbar-toggle ml-3" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
