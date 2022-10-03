9578c6505521458ca2259220ffdebc098e76d604
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-03 08:56:16 -0700

    Added NavBar component and styles, added styles to root div

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/NavBar.css';
+
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top d-flex">
+            <button className="navbar-toggler navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav d-flex ml-auto mx-auto">
+                    <li className="nav-item">
+                        <a href="#home-projects-section" className="nav-link px-4 text-center">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#home-skills-section" className="nav-link px-4 text-center">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#home-about-section" className="nav-link px-4 text-center">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#home-contact-section" className="nav-link px-4 text-center">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
