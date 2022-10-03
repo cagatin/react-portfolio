ec0e4a630989fb042f08dbc1595157cd66817a6c
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-03 09:09:53 -0700

    Added Hero Component to display Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Hero.css';
+
+export default function Hero() {
+    return (
+        <section id="hero-container-section" className="main-section">
+            <div id="hero-container" className="typewriter">
+                <h1 id="desktop-title">Felix Cagatin. Web Developer.</h1>
+            </div>
+        </section>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from '../components/Navbar';
-
+import Hero from '../components/Hero'
 
 export default function Home() {
     return (
         <div>
             <NavBar />
+            <Hero />
         </div>
     )
 }
